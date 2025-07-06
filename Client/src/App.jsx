14beaf80f3810c4afcceb4a99fa084a0000cf01f
CODER-145 <hammadshahzad13@gmail.com>
2025-07-06 14:27:02 +0500

    added ptotectedroutes and app.jsx edited

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,36 @@
-import { Route, Routes } from 'react-router-dom'
-import HomePage from './pages/dashboard/HomePage'
-import './styles/main.css'
-import LoginPage from './pages/auth/LoginPage'
-import MainRoutes from './Routes/MainRoutes'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/dashboard/HomePage';
+import LoginPage from './pages/auth/LoginPage';
+import FeedPage from './pages/feed/FeedPage'; 
+import ProtectedRoute from './Routes/protectedRoutes';
+import './styles/main.css';
 
 function App() {
-  return(
-      <MainRoutes/>
-  )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+
+        <Route
+          path="/feed"
+          element={
+            <ProtectedRoute>
+              <FeedPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
